1a5c36af074b65278241596cc259b52d782d4f03
jihchi <achi@987.tw>
2022-06-15 04:31:04 -0400

    Install chakra-ui

---
+++ shared/theme.ts
@@ -0,0 +1,9 @@
+import { type ColorModeScriptProps } from "@chakra-ui/react";
+
+const theme = {
+  config: {
+    initialColorMode: "light" as ColorModeScriptProps["initialColorMode"],
+  },
+};
+
+export default theme;
